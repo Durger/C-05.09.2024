cb3592d047b7c684bd65c582f90afe8b68b8b21c
Durger <149479760+Durger@users.noreply.github.com>
2024-09-05 21:27:20 +0200

    Add files via upload

---
+++ Lesson_2_05.09.2024/Lesson_2_05.09.2024/Lesson_2_05.09.2024.cpp
@@ -0,0 +1,101 @@
+﻿// Lesson_2_05.09.2024.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+void main()
+{
+	system("сhcp 1251");
+	
+	//int v=1; // размер 4 байта;
+	//char c='v'; // размер 1 байта;
+	////переменные только с маленьких букв-в нижнем регистре
+	//v=2;
+	//const int d=54;
+	//double f;
+	//f=4.5;
+	//cout<<c<< "\n" << v << "\n" <<f<< endl;
+
+	//cout<<"My name is:"<<"\n"<<"Den"<<"\n"<<"My age is:"<<"\n" << "35";
+
+
+	/*cout << "\n";
+	cout << endl;
+	int dayIn2000Year = 366;
+	int hourInDay = 24;
+	int hourIn2000Year = dayIn2000Year * hourInDay;
+	cout <<"\t\t hourIn2000Year="<<hourIn2000Year<<" hours";*/
+
+	/*float discount=0.05;
+	float cost=10.50;
+	int count=5;
+	float price=count*cost-count*cost*discount;
+	cout<<price;
+
+	bool flag=true;
+	char symbol = '&';*/
+
+	//int age;
+	//string name;
+	//cout << "Enter your name:\n";
+	//cin >> name;
+	//cout << "Enter your age:\n";
+	//cin >> age;
+	//system("cls");//Очитска консоли
+	//cout << "Your name is:\n" << name<<"\n";
+	//cout << "Your age is:\n" << age;
+
+	//int count;//количество
+	//float cost = 10.50;//цена
+	//float discount;//скидка
+	//cout << "Enter count:\n";
+	//cin >> count;
+	//cout << "Enter discount:\n";
+	//cin >> discount;
+	//system("cls");//Очитска консоли
+	//float price = (count * cost) - (count * cost * discount);
+	//cout << "Your price:\n" << price << "\n";
+
+	/*cout << "Let's play: \n";
+	int i;
+	cout << "Enter a number";
+	cin >> i;
+	cout << "I have: " << i + 1 << endl;
+	cout << "I'm a winner!!!\n";*/
+
+	//i++ - увеличение значения на 1 единичу с перезаписыванием первоначального значения
+
+	/*int a = 8;
+	cout << a << endl;
+	a++;
+	cout << a << endl;*/
+
+	//int a1 = 8;
+	//cout << a1 << endl;
+	//a--;//a1-1
+	//a1--;//a-1 ИЛИ a=a-1
+	//cout << a1 <<"\n" <<a<< endl;
+
+	int age;
+	cout << "Enter your age:\n";
+	cin >> age;
+	age++;
+	cout << "Your age is:\n" << age;
+	int ageMinus;
+	cout << "Enter your age:\n";
+	cin >> ageMinus;
+	ageMinus--;
+	cout << "Your age is:\n" << ageMinus;
+
+
+
+
+
+	
+
+
+
+
+
+}
+
